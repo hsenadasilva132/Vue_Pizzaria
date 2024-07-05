c087147c894633b12ba911a7abec5b5aa3af55e8
Henrique Sena <135704597+hsenadasilva132@users.noreply.github.com>
2024-07-05 00:16:08 -0300

    Componente Navbar
    Adicionado o componente Navbar, incluso o componente de Pedidos e a imagem do logo

---
+++ vue_pizzaria/src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+       <nav>
+        <router-link to="/"> <!--to definido com barra para indicar que ao clicar no logo vai direcionar para o home-->
+            <img src="" alt="">
+        </router-link>
+           <router-link to="/">Home</router-link> |
+           <router-link to="/Pedidos">Pedidos</router-link>
+      </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
+
+<style>
+    
+</style>
