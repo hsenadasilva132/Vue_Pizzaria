19b51e873d1b2ffdb47b0ee874c535c62dff80ae
Henrique Sena <135704597+hsenadasilva132@users.noreply.github.com>
2024-07-06 12:35:07 -0300

    Estilização de App
    Adicionado estilização global em App

--- vue_pizzaria/src/App.vue
+++ vue_pizzaria/src/App.vue
@@ -28,5 +28,22 @@ export default {
 
 
 <style>
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  .main-container {
+    margin: 50px;
+    min-height: 400px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
