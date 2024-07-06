91d00203b8e49686a67e0daed0b573cc2dcb7193
Henrique Sena <135704597+hsenadasilva132@users.noreply.github.com>
2024-07-06 12:33:28 -0300

    Estilização de Navbar
    Adicionado estilização de Navbar em scoped

--- vue_pizzaria/src/components/Navbar.vue
+++ vue_pizzaria/src/components/Navbar.vue
@@ -17,6 +17,34 @@ export default {
 }
 </script>
 
-<style>
-    
+<style scoped>
+      nav {
+    background-color: #222;
+    border-bottom: 4px solid #111;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+   }
+
+   #logo-url {
+    margin: auto;
+    margin-left: 0;
+   }
+
+   img {
+    width: 40px;
+    height: 40px;
+   }
+
+   nav a {
+    color: #FCDA03;
+    text-decoration: none;
+    margin: 12px;
+    transition: .5s;
+   }
+
+   nav a:hover {
+    color: #FFF;
+   }
 </style>
