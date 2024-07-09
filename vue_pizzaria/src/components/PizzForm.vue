35cbbfd8f5d005aba1b6d0306641c32b597c1956
Henrique Sena <135704597+hsenadasilva132@users.noreply.github.com>
2024-07-09 20:59:35 -0300

    Requisição de opcionais
    Requisição de opcionais bem sucedida. 09/07/24 - 20:59

---
+++ vue_pizzaria/src/components/PizzForm.vue
@@ -0,0 +1,11 @@
+                    <div id="opcionais-container" class="input-container">
+                        <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
+                        <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
+                            <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.data">
+                            <span>{{ opcional.tipo }}</span>
+                        </div>         
+            opcionaisdata: null,
+            opcionais: [],
+            const req = await fetch("http://localhost:3000/ingredientes"); //Esse código faz uma requisição assíncrona e aguardando a resposta
+            const data = await req.json(); //Esperando resolver a requisição, transformar em json para poder ter dados.
+            this.opcionaisdata = data.opcionais;
