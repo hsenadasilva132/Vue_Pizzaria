3ece629b5e282440bb4492b9fd44f0d254c3f79f
Henrique Sena <135704597+hsenadasilva132@users.noreply.github.com>
2024-07-12 23:26:26 -0300

    Update HomeView.vue
    Adições iniciais de Home

--- vue_pizzaria/src/views/HomeView.vue
+++ vue_pizzaria/src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="home">
-    <h1>HOME</h1>
+  <div>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte a sua Pizza</h1>
+    <PizzForm />
   </div>
+</div>
 </template>
 
 <script>
+import Banner from '../components/Banner.vue'
+import PizzForm from '../components/PizzForm.vue'
+
+export default {
+  name: "Home",
+  components: {
+    Banner,
+    PizzForm
+  }
+}
 
 </script>
